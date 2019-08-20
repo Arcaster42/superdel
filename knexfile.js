7de829c35fc26251f265045003fe7f9ff9ad6596
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-20 17:05:06 +0900

    knexfile config with dev, stage, prod options

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,55 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
+	
+	development: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL || {
+			host: process.env.DB_HOST, 
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD, 
+			database: process.env.DB_NAME,
+			ssl: true
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		},
+		seeds: {
+			directory: __dirname + '/seeds/development'
+		}
+	
+	},
 
-  development: {
-    client: 'pg',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+	staging: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL || {
+			host: process.env.DB_HOST,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			ssl: true
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		},
+		seeds: {
+			directory: __dirname + '/seeds/development'
+		}
+	},
 
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
+}
