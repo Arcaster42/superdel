4c7a6f8d4f7037d1f84de7e87dc7c59e8e0f416a
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-20 17:41:50 +0900

    implemented register user

--- queries.js
+++ queries.js
@@ -5,8 +5,25 @@ const db = require('knex')(knexFile)
 const bcrypt = require('bcrypt')
 
 const registerUser = (userObj) => {
-
-}
+  return db('users').where('email', userObj.email)
+    .then((results) => {
+        if (results.length < 1) {
+            return new Promise((resolve, reject) => {
+                bcrypt.hash(userObj.password, 37, (err, hash) => {
+                    if (err) reject(err)
+                    return db('users').insert({ email: userObj.email, pass_hash: hash })
+                        .then(() => {
+                            userObj.password = null
+                            resolve(userObj)
+                        })
+                        .catch((err) => reject(err))
+                })
+            })
+        }
+        else return { err: 'Username Unavailable' }
+    })
+    .catch((err) => { return err })
+ }
 
 module.exports = {
     registerUser
