d6c4fb0828d61ff8756c73f8c4a6907c7067778b
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-20 17:59:14 +0900

    finished two queries loginUser, register

--- queries.js
+++ queries.js
@@ -25,6 +25,26 @@ const registerUser = (userObj) => {
     .catch((err) => { return err })
  }
 
+const loginUser = (userObj) => {
+    return db('users').where('email', userObj.email)
+      .then((results) => {
+        if (results.length === 1) {
+          const user = results[0]
+            return new Promise((resolve, reject) => {
+            bcrypt.compare(userObj.password, user.pass_hash, (err, same)=> {
+              if (err) reject(err)
+              if(!same) reject({ err: 'Invalid Credentials' })
+              if (same) {
+                userObj.password = null
+                resolve(userObj) 
+            }})
+          })
+        }
+        else return { err: 'Username Not Found' }
+      }).catch((err) => { return err })
+} 
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
