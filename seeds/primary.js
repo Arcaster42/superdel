553f811bf2a2e49d1cdf083458a3109329e20d9b
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-21 10:19:38 +0900

    added seed data for 1 staff, 2 users

--- seeds/primary.js
+++ seeds/primary.js
@@ -1,13 +1,48 @@
 
+/*
+return knex.schema.createTable('users', t => {
+			t.string('email').primary()
+			t.string('first_name')
+			t.string('last_name')
+			t.string('address')
+			t.string('pass_hash')
+			t.boolean('staff')
+		})
+*/
+
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('users').insert([
+				{
+					email: 'user1@email.com',
+					first_name: 'John-1',
+					last_name: 'Doe-1',
+					address:
+						'B2 Vort, 3 Chome-1-35 Motoazabu, Minato City, Tokyo 106-0046',
+					pass_hash: 'password-1',
+					staff: true
+				},
+				{
+					email: 'user2@email.com',
+					first_name: 'John-2',
+					last_name: 'Doe-2',
+					address:
+						'B2 Vort, 3 Chome-2-35 Motoazabu, Minato City, Tokyo 106-0046',
+					pass_hash: 'password-2',
+					staff: false
+				},
+				{
+					email: 'staff3@email.com',
+					first_name: 'John-3',
+					last_name: 'Doe-3',
+					address:
+						'B2 Vort, 3 Chome-1-35 Motoazabu, Minato City, Tokyo 106-0046',
+					pass_hash: 'password-3',
+					staff: false
+				}
+			])
+    })
+}
