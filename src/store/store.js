af536d6bf4db5b6c533990a645a281f3654014f5
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-21 22:47:09 +0900

    added setcartItems and two new states

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,18 @@
-import Vuex from 'vuex';
-import Vue from 'vue';
+import Vuex from 'vuex'
+import Vue from 'vue'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: null,
+    user: 'Ryohei',
     loginView: 'login',
-    // mainView: 'Login',
-    mainView: 'StaffLandingPage',
+    mainView: 'UserLandingPage',
     driverView: 'all',
     driverMyOrders: [],
     driverSelectedOrders: [],
+    userCartItems: [],
+    userCartQuantities: [],
   },
 
   getters: {
@@ -38,9 +39,14 @@ export default new Vuex.Store({
       state.driverSelectedOrders.push(value)
     },
     sendSelectedOrders(state, value) {
-      state.driverMyOrders = value;
-      state.driverView = 'my';
-    }
+      state.driverMyOrders = value
+      state.driverView = 'my'
+    },
+    setCartItems(state, itemObj) {
+      state.userCartItems = itemObj.itemArray
+      state.userCartQuantities =itemObj.quantitiesArray
+    },
+
   },
 
   actions: {
