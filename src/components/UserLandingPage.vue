7714850fb52c2642272769c5d93127499b72fd12
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-21 22:47:28 +0900

    crushing vue like a boss

--- src/components/UserLandingPage.vue
+++ src/components/UserLandingPage.vue
@@ -34,59 +34,118 @@
 
     <div class="row align-items-center justify-content-center text-center">
       <div class="col-lg-10 align-self-end">
-        <h1 class="text-black font-weight-bold">Welcome, ${Driver}</h1>
+        <hr class="divider my-4" />
+        <h1 class="text-black font-weight-bold">Welcome, {{user}}</h1>
         <hr class="divider my-4" />
       </div>
     </div>
 
-    <!-- ALL ORDERS -->
-    <div v-if="driverView === 'all'">
-      <b-col lg="4" class="pb-2">
-        <b-button size="lg" @click="$store.commit('sendSelectedOrders', driverSelectedOrders)">SuperDel Selected Order(s)</b-button>
-      </b-col>
-
-      <b-form-checkbox
-        v-for="item in items"
-        :key="item.key"
-        href="#"
-        class="flex-column align-items-start"
-        :checked="itemClick(item)"
-        variant="secondary"
-      >
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">{{item.one}}</h5>
-          <small class="text-muted">{{item.two}}</small>
+<!-- USER Cart-->
+<div>
+  <b-list-group>
+    <b-card bg-variant="dark" text-variant="white" title="Checkout">
+      <b-card-text>
+        Thank you for shopping with us, {{user}}!
+        <div class="float-right">
+        <b-button href="#" variant="primary" v-on:click="checkout">Checkout</b-button>
         </div>
-        <p class="mb-1">{{item.three}}</p>
-        <small class="text-muted">{{item.four}}</small>
-      </b-form-checkbox>
+      </b-card-text>
+    </b-card>
 
+     <b-list-group flush>
+       <div v-for="(item, index) in cartItems" v-bind:key="index">
+      <b-list-group-item variant="secondary" href="#">Product: {{  item }}
+        <div>Quantity: {{cartQuantities[index] }}</div></b-list-group-item>
+       </div>
+    </b-list-group>
+  </b-list-group>
+</div>
+
+<!-- USER ORDER Card-->
+    <div>
+      <b-card-group columns>
+  <div v-for="(item, index) in groceryItems" v-bind:key="index">
+  <b-card
+    :title="item.productTitle"
+    :img-src="item.photo"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem; width: 100%"
+    class="mb-2"
+    :key="index"
+  >
+    <b-card-text>
+      {{ item.productDetails}}
+      <div>
+        <b-card-text class="text-muted float-right" >{{item.productPrice}}</b-card-text>
+      </div>
+    </b-card-text>
+
+    <b-button href="#" variant="primary" v-on:click="addItem(item.productTitle)">Add to Cart</b-button>
+
+
+  </b-card>
     </div>
+    </b-card-group>
+</div>
+
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState } from 'vuex'
+import basil from '../assets/basil.jpeg'
+import milk from '../assets/milk.jpeg'
+import mozza from '../assets/mozza.jpeg'
+import pasta from '../assets/pasta.jpeg'
+import sauce from '../assets/sauce.jpeg'
+import tomato from '../assets/tomato.jpeg'
 
 export default {
-  name: "UserLandingPage",
-  computed: mapState(["driverView", "driverSelectedOrders", "driverMyOrders"]),
+  name: 'UserLandingPage',
+  computed: mapState(['user', 'driverSelectedOrders', 'driverMyOrders']),
   data: () => ({
     items: [
-      { key: 1, one: "qwer", two: "poi", three: "1234", four: "098^"},
-      { key: 2, one: "owetjhwi3t", two: "poi", three: "1234", four: "098^"}
+      { key: 1, one: 'qwer', two: 'poi', three: '1234', four: '098^'},
+      { key: 2, one: 'owetjhwi3t', two: 'poi', three: '1234', four: '098^'}
     ],
-    selectedOrders: [],
-    isActive: {}
+    cartItems: ['milk', 'basil'],
+    cartQuantities: [1, 2],
+    isActive: {},
+    groceryItems: [
+      { productTitle: 'Milk', photo: milk, productDetails: 'poi', productPrice: '$2.34'},
+      { productTitle: 'Mozarella', photo: mozza, productDetails: 'poi', productPrice: '$2.35'},
+      { productTitle: 'Tomatoes', photo: tomato, productDetails: 'poi', productPrice: '$2.36'},
+      { productTitle: 'Pasta', photo: pasta, productDetails: 'poi', productPrice: '$2.37'},
+      { productTitle: 'Basil', photo: basil, productDetails: 'poi', productPrice: '$2.38'},
+      { productTitle: 'Sauce', photo: sauce, productDetails: 'poi', productPrice: '$2.38'},
+    ]
   }),
   methods: {
-    itemClick(item) {
+    itemClick: function(item) {
       console.log(item)
       this.$store.commit('selectOrder', item)
       this.isActive[item.key] = 1
     },
-  }
-};
+    checkout: function() {
+      const itemObj = { itemArray: this.cartItems , quantitiesArray: this.cartQuantities}
+      this.$store.commit('setCartItems', itemObj)
+    },
+    addItem: function(itemIndex) {
+      // if (this.cartItems.length) {
+      //   for (let i = 0; i <this.cartItems.length; i++) {
+      //     if (this.cartItems[i] === this.groceryItems[itemIndex].productTitle) {
+      //       this.cartQuantities[i] = this.cartQuantities[i] + 1
+      //       break
+      //     }
+      //   }
+      // }
+      // this.cartItems.push( this.groceryItems[itemIndex].productTitle)
+      // this.cartQuantities.push(1)
+    }
+  },
+}
 </script>
 
 <style scoped>
