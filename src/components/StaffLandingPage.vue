6c77052a199fb9f15bbc204e1ac4f6dc030bc28c
Lucas <kimlucas@gmail.com>
2019-08-21 14:27:10 +0900

    staff landingPage now has allOrders, myOrders

---
+++ src/components/StaffLandingPage.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">SuperDel</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#" @click="$store.commit('staffPage', 'all')">All Orders</b-nav-item>
+          <b-nav-item href="#" @click="$store.commit('staffPage', 'my')">My Orders</b-nav-item>
+          <b-nav-item href="#" @click="$store.commit('staffPage', 'completed')">Completed</b-nav-item>
+          <b-nav-item href="#">Map</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">JP</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content">
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#" @click="$store.commit('staffPage', 'completed')">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <div class="row align-items-center justify-content-center text-center">
+      <div class="col-lg-10 align-self-end">
+        <h1 class="text-black font-weight-bold">Welcome, ${Driver}</h1>
+        <hr class="divider my-4" />
+      </div>
+      <!-- <div class="col-lg-8 align-self-baseline">
+        <p class="text-white-75 mb-5">Available orders</p>
+      </div>-->
+    </div>
+
+    <!-- ALL ORDERS -->
+    <div v-if="driverView === 'all'">
+      <b-col lg="4" class="pb-2">
+        <b-button size="lg">Start Order(s)</b-button>
+      </b-col>
+
+      <b-list-group-item
+        v-for="item in items"
+        :key="item.key"
+        href="#"
+        class="flex-column align-items-start"
+        @click="$store.commit('selectOrder', item)"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{item.one}}</h5>
+          <small class="text-muted">{{item.two}}</small>
+        </div>
+        <p class="mb-1">{{item.three}}</p>
+        <small class="text-muted">{{item.four}}</small>
+      </b-list-group-item>
+    </div>
+
+    <!-- MY ORDERS -->
+    <div v-if="driverView === 'my'">
+      <b-list-group-item
+        v-for="data in driverMyOrders"
+        :key="data.key"
+        href="#"
+        class="flex-column align-items-start"
+        @click="$store.commit('selectOrder', data.key)"
+      >
+        <div class="d-flex w-100 justify-content-between">
+          <h5 class="mb-1">{{data.one}}</h5>
+          <small class="text-muted">{{data.two}}</small>
+        </div>
+        <p class="mb-1">{{data.three}}</p>
+        <small class="text-muted">{{data.four}}</small>
+      </b-list-group-item>
+    </div>
+
+    <!-- COMPLETED ORDERS -->
+    <b-list-group-item
+      v-if="driverView === 'completed'"
+      href="#"
+      class="flex-column align-items-start"
+    >
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">ONE</h5>
+        <small class="text-muted">3 minutes ago</small>
+      </div>
+      <p class="mb-1">Egg, Milk, Tea</p>
+      <small class="text-muted">¥3,500</small>
+    </b-list-group-item>
+    <b-list-group-item
+      v-if="driverView === 'completed'"
+      href="#"
+      class="flex-column align-items-start"
+    >
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">TWO</h5>
+        <small class="text-muted">3 minutes ago</small>
+      </div>
+      <p class="mb-1">Egg, Milk, Tea</p>
+      <small class="text-muted">¥3,500</small>
+    </b-list-group-item>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "StaffLandingPage",
+  computed: mapState(["driverView", "driverMyOrders"]),
+  data: () => ({
+    items: [
+      { key: 1, one: "qwer", two: "poi", three: "1234", four: "098^" },
+      { key: 2, one: "owetjhwi3t", two: "poi", three: "1234", four: "098^" }
+    ]
+  })
+};
+</script>
+
+<style scoped>
+</style>
