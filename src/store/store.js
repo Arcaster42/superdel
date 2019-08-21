9071f5bd09d6e2dbca1beab52a822e9018ff435e
Lucas <kimlucas@gmail.com>
2019-08-21 14:35:31 +0900

    added states and mutations for staffLandingPage

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,39 @@
-import Vuex from 'vuex'
-import Vue from 'vue'
+import Vuex from 'vuex';
+import Vue from 'vue';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     user: null,
-    loginView: 'userType'
+    loginView: 'userType',
+    driverView: 'all',
+    driverMyOrders: [],
+    mainView: 'Login'
   },
-  
+
   getters: {
     // Here we will create any getters
   },
-  
+
   mutations: {
     changeLoginView(state, view) {
-        state.loginView = view
+      state.loginView = view
     },
     setUser(state, user) {
-        state.user = user
+      state.user = user
+    },
+    staffPage(state, value) {
+      state.driverView = value
+    },
+    selectOrder(state, value) {
+      if (state.driverMyOrders.includes(value)) {
+        return
+      }
+      state.driverMyOrders.push(value)
     }
   },
-  
+
   actions: {
     // Here we will create any actions
   }
