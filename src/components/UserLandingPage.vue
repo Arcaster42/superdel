4bff266a0fb0c1be25389911e634f1d108b90599
Ryohei Watanabe <watanaber17@gmail.com>
2019-08-22 00:23:27 +0900

    added options for product categories

--- src/components/UserLandingPage.vue
+++ src/components/UserLandingPage.vue
@@ -40,8 +40,9 @@
       </div>
     </div>
 
+
 <!-- USER Cart-->
-<div>
+<div class="cart">
   <b-list-group>
     <b-card bg-variant="dark" text-variant="white" title="Checkout">
       <b-card-text>
@@ -55,42 +56,103 @@
      <b-list-group flush>
        <div v-for="(item, index) in cartItems" v-bind:key="index">
       <b-list-group-item variant="secondary" href="#">Product: {{  item }}
-        <div>Quantity: {{cartQuantities[index] }}</div></b-list-group-item>
+        <div>Quantity: {{computeCartQuantities[index] }}</div></b-list-group-item>
        </div>
     </b-list-group>
   </b-list-group>
 </div>
 
 <!-- USER ORDER Card-->
-    <div>
-      <b-card-group columns>
-  <div v-for="(item, index) in groceryItems" v-bind:key="index">
-  <b-card
-    :title="item.productTitle"
-    :img-src="item.photo"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem; width: 100%"
-    class="mb-2"
-    :key="index"
-  >
-    <b-card-text>
-      {{ item.productDetails}}
-      <div>
-        <b-card-text class="text-muted float-right" >{{item.productPrice}}</b-card-text>
-      </div>
-    </b-card-text>
-
-    <b-button href="#" variant="primary" v-on:click="addItem(item.productTitle)">Add to Cart</b-button>
+    <div class="options">
 
 
-  </b-card>
+<!-- product nav-->
+    <div class="options-nav">
+      <b-nav pills fill>
+        <b-nav-item v-bind:active="productView === 'Italian'" v-on:click="productView = 'Italian'">Italian</b-nav-item>
+        <b-nav-item v-bind:active="productView === 'Japanese'" v-on:click="productView = 'Japanese'">Japanese</b-nav-item>
+        <b-nav-item v-bind:active="productView === 'Basics'" v-on:click="productView = 'Basics'">Basics</b-nav-item>
+      </b-nav>
     </div>
-    </b-card-group>
-</div>
 
+    <div v-if="productView==='Italian'">
+    <b-card-group columns>
+      <div v-for="(item, index) in groceryItems.italian" v-bind:key="index">
+      <b-card
+        :title="item.productTitle"
+        :img-src="item.photo"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem; width: 100%"
+        class="mb-2"
+        :key="index"
+      >
+        <b-card-text>
+          {{ item.productDetails}}
+          <div>
+            <b-card-text class="text-muted float-right" >${{item.productPrice}}</b-card-text>
+          </div>
+        </b-card-text>
+        <b-button href="#" variant="primary" v-on:click="addItem(item.productTitle)">Add to Cart</b-button>
+      </b-card>
+        </div>
+    </b-card-group>
   </div>
+
+  <div v-if="productView==='Japanese'">
+    <b-card-group columns>
+      <div v-for="(item, index) in groceryItems.japanese" v-bind:key="index">
+      <b-card
+        :title="item.productTitle"
+        :img-src="item.photo"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem; width: 100%"
+        class="mb-2"
+        :key="index"
+      >
+        <b-card-text>
+          {{ item.productDetails}}
+          <div>
+            <b-card-text class="text-muted float-right" >${{item.productPrice}}</b-card-text>
+          </div>
+        </b-card-text>
+        <b-button href="#" variant="primary" v-on:click="addItem(item.productTitle)">Add to Cart</b-button>
+      </b-card>
+        </div>
+    </b-card-group>
+  </div>
+  
+<div v-if="productView==='Basics'">
+    <b-card-group columns>
+      <div v-for="(item, index) in groceryItems.basics" v-bind:key="index">
+      <b-card
+        :title="item.productTitle"
+        :img-src="item.photo"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem; width: 100%"
+        class="mb-2"
+        :key="index"
+      >
+        <b-card-text>
+          {{ item.productDetails}}
+          <div>
+            <b-card-text class="text-muted float-right" >${{item.productPrice}}</b-card-text>
+          </div>
+        </b-card-text>
+        <b-button href="#" variant="primary" v-on:click="addItem(item.productTitle)">Add to Cart</b-button>
+      </b-card>
+        </div>
+    </b-card-group>
+  </div>
+  </div>
+  
+
+</div>
 </template>
 
 <script>
@@ -104,23 +166,40 @@ import tomato from '../assets/tomato.jpeg'
 
 export default {
   name: 'UserLandingPage',
-  computed: mapState(['user', 'driverSelectedOrders', 'driverMyOrders']),
+  computed: {
+    ...mapState(['user', 'driverSelectedOrders', 'driverMyOrders']),
+    computeCartQuantities: function() {
+      return this.cartQuantities
+    }},
   data: () => ({
-    items: [
-      { key: 1, one: 'qwer', two: 'poi', three: '1234', four: '098^'},
-      { key: 2, one: 'owetjhwi3t', two: 'poi', three: '1234', four: '098^'}
+    cartItems: [],
+    cartQuantities: [],
+    productView: "Italian",
+    groceryItems: {
+      italian: [
+      { productTitle: 'Milk', photo: milk, productDetails: 'poi', productPrice: 2.34},
+      { productTitle: 'Mozarella', photo: mozza, productDetails: 'poi', productPrice: 2.35},
+      { productTitle: 'Tomatoes', photo: tomato, productDetails: 'poi', productPrice: 2.36},
+      { productTitle: 'Pasta', photo: pasta, productDetails: 'poi', productPrice: 2.37},
+      { productTitle: 'Basil', photo: basil, productDetails: 'poi', productPrice: 2.38},
+      { productTitle: 'Sauce', photo: sauce, productDetails: 'poi', productPrice: 2.38},
     ],
-    cartItems: ['milk', 'basil'],
-    cartQuantities: [1, 2],
-    isActive: {},
-    groceryItems: [
-      { productTitle: 'Milk', photo: milk, productDetails: 'poi', productPrice: '$2.34'},
-      { productTitle: 'Mozarella', photo: mozza, productDetails: 'poi', productPrice: '$2.35'},
-      { productTitle: 'Tomatoes', photo: tomato, productDetails: 'poi', productPrice: '$2.36'},
-      { productTitle: 'Pasta', photo: pasta, productDetails: 'poi', productPrice: '$2.37'},
-      { productTitle: 'Basil', photo: basil, productDetails: 'poi', productPrice: '$2.38'},
-      { productTitle: 'Sauce', photo: sauce, productDetails: 'poi', productPrice: '$2.38'},
-    ]
+    basics: [
+      { productTitle: 'Milk', photo: milk, productDetails: 'poi', productPrice: 2.34},
+      { productTitle: 'Mozarella', photo: mozza, productDetails: 'poi', productPrice: 2.35},
+      { productTitle: 'Tomatoes', photo: tomato, productDetails: 'poi', productPrice: 2.36},
+      { productTitle: 'Pasta', photo: pasta, productDetails: 'poi', productPrice: 2.37},
+      { productTitle: 'Basil', photo: basil, productDetails: 'poi', productPrice: 2.38},
+      { productTitle: 'Sauce', photo: sauce, productDetails: 'poi', productPrice: 2.38}
+    ],
+    japanese: [
+      { productTitle: '牛乳', photo: milk, productDetails: 'poi', productPrice: 2.34},
+      { productTitle: 'Mozarella', photo: mozza, productDetails: 'poi', productPrice: 2.35},
+      { productTitle: 'Tomatoes', photo: tomato, productDetails: 'poi', productPrice: 2.36},
+      { productTitle: 'Pasta', photo: pasta, productDetails: 'poi', productPrice: 2.37},
+      { productTitle: 'Basil', photo: basil, productDetails: 'poi', productPrice: 2.38},
+      { productTitle: 'Sauce', photo: sauce, productDetails: 'poi', productPrice: 2.38}
+    ]}
   }),
   methods: {
     itemClick: function(item) {
@@ -132,21 +211,43 @@ export default {
       const itemObj = { itemArray: this.cartItems , quantitiesArray: this.cartQuantities}
       this.$store.commit('setCartItems', itemObj)
     },
-    addItem: function(itemIndex) {
-      // if (this.cartItems.length) {
-      //   for (let i = 0; i <this.cartItems.length; i++) {
-      //     if (this.cartItems[i] === this.groceryItems[itemIndex].productTitle) {
-      //       this.cartQuantities[i] = this.cartQuantities[i] + 1
-      //       break
-      //     }
-      //   }
-      // }
-      // this.cartItems.push( this.groceryItems[itemIndex].productTitle)
-      // this.cartQuantities.push(1)
+    addItem: function(item) {
+      if (this.cartItems.length) {
+        for (let i = 0; i <this.cartItems.length; i++) {
+          if (this.cartItems[i] === item) {
+            this.cartQuantities.splice(i, 1, this.cartQuantities[i] + 1)
+            break
+          }
+          else if (i === this.cartItems.length-1 && this.cartItems[i] !==item) {
+            this.cartItems.push(item)
+            this.cartQuantities.push(1)
+            break
+          }
+        }
+      }
+      else {
+        this.cartItems.push(item)
+        this.cartQuantities.push(1)
+      }
     }
   },
 }
 </script>
 
 <style scoped>
+.options{
+  width: 50%;
+  float: left;
+  height:100vh;
+}
+.cart {
+  width:50%;
+  float: right;
+  padding-left: 5vw;
+}
+
+.options-nav{
+  width: 100%;
+  padding-bottom: 5vh;
+}
 </style>
